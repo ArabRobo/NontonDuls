3f65ec6dbe6f2523b00089de54fa1d66d011d3a4
ArabRobo <121577164+ArabRobo@users.noreply.github.com>
2023-10-17 15:37:57 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6678049115:AAH5Ccmf4GeG0MEt0v7GXoxb_TwU4PMOpOU")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6534958212:AAFZ_NtLnSjPpV8RUIXR4K2EyW6HzsCL3HQ")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "10064016"))
@@ -20,7 +20,7 @@ APP_ID = int(os.environ.get("APP_ID", "10064016"))
 API_HASH = os.environ.get("API_HASH", "b9ca5d9a6c625a890af28db4adf50cf4")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001932073209"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001839380945"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "Dhilnihnge")
@@ -30,13 +30,13 @@ HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://krlilecu:G1c7ZBE3NCoYnNUhX28lHizWXEewVfW7@trumpet.db.elephantsql.com/krlilecu")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://wpvxrvmo:n6vJyWstswAifaaZoR4C12dE5TUojQ6P@trumpet.db.elephantsql.com/wpvxrvmo")
 
 PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1001962989739"))
-FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "-1001984442502"))
+FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1001757481471"))
+FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "-1001809708760"))
 FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "0"))
 FORCE_SUB_4 = int(os.environ.get("FORCE_SUB_4", "0"))
 FORCE_SUB_5 = int(os.environ.get("FORCE_SUB_5", "0"))
